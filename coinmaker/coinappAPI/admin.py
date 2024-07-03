a86549088c4f16d5e329d01a7130a992ccfd8af9
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-07-04 00:50:01 +0500

    barcha modellar admin panelga registratsiya qilindi

--- coinmaker/coinappAPI/admin.py
+++ coinmaker/coinappAPI/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User, InvitedFriends, TelegramData, Tasks
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    all = ['level', 'balance', 'boots', 'last_updated']
+    list_display, list_filter, search_fields = all, all, all
+
+
+@admin.register(InvitedFriends)
+class InvitedFriendsAdmin(admin.ModelAdmin):
+    all = ['name', 'telegram_id']
+    list_display, list_filter, search_fields = all, all, all
+
+
+@admin.register(TelegramData)
+class TelegramDataAdmin(admin.ModelAdmin):
+    all = ['name', 'telegram_id']
+    list_display, list_filter, search_fields = all, all, all
+
+
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    all = ['task', 'level', 'bonus']
+    list_display, list_filter, search_fields = all, all, all
