28420935f36bb5e809733d70197199e2ed7ad6b7
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-07-04 00:50:44 +0500

    viewlarni yozish kerak

--- coinmaker/coinappAPI/views.py
+++ coinmaker/coinappAPI/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import User, InvitedFriends, TelegramData, Tasks
+from .serializers import UserSerializer, InvitedFriendsSerializer, TelegramDataSerializer, TasksSerializer
+from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserAPIView(APIView):
+    pass
+
+
+class InvitedFriendsAPIView(APIView):
+    pass
+
+
+class TelegramDataAPIView(APIView):
+    pass
+
+
+class TasksAPIView(APIView):
+    pass
